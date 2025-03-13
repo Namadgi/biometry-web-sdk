2fb2b6729715871884a9e2286ed5a4a42cb1056b
cr1m1 <kalysbekovz@gmail.com>
2025-03-13 11:54:54 +0600

    Fix import errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './sdk';
-export * from './types';
+export * from './types/index';
 export { BiometryEnrollment } from './components/biometry-enrollment';
 export { ProcessVideoComponent } from './components/process-video';
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from './biometry/consent';
+export * from './biometry/doc-auth';
+export * from './biometry/enrollment';
+export * from './biometry/face-match';
+export * from './biometry/process-video';
+export * from './biometry/session';
+export * from './internal';
