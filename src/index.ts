4788cca0241a25d0fbc4ea9b57d5cd1a89632025
trekvartista <isken_150502@mail.ru>
2025-05-28 14:14:52 +0600

    Fix biometry sdk export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './sdk';
+export * from './sdk/index';
 export * from './types/index';
 export { BiometryEnrollment } from './ui/biometry-enrollment';
 export { ProcessVideoComponent } from './ui/process-video';
