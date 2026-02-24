cd2d83df3b1cbd117011ad2fca302e4e5d7761e3
trekvartista <isken_150502@mail.ru>
2026-02-25 00:48:04 +0600

    Fix face match response type

--- src/types/biometry/face-match.ts
+++ src/types/biometry/face-match.ts
@@ -1,4 +1,11 @@
 export interface FaceMatchResponse {
+  data: FaceMatchData;
+  scoring_result: Record<string, any>;
+  decision_reasons: string[];
+  message: string;
+}
+
+export interface FaceMatchData {
   code: number;
   result: number;
   description: string;
